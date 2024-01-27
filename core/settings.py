d610e0126c4bc65edf2f615cac70254f4845f2e7
Nedyalko <nedcoder@gmail.com>
2024-01-28 00:29:29 +0200

    new changes

--- core/settings.py
+++ core/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-if os.environ.get('DEBUG') == False:
-    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
-else:
-    ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 
