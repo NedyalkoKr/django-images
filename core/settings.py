70f9823c8de90de3f7b7837cf8b915fed5fd8c84
Nedyalko <nedcoder@gmail.com>
2024-01-29 00:17:43 +0200

    new changes

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
