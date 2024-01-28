9df4055646f2c5da7ec3efb748062e5b42805812
Nedyalko <nedcoder@gmail.com>
2024-01-28 15:23:57 +0200

    new changes

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
+ALLOWED_HOSTS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 
 # Application definition
 
